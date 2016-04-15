331d36844da6fdac25cea71cffc42a5cd4fe835d
大畠 裕 <0FR108021@local.furyu.jp>
2016-04-15 15:20:56 +0900

    チャット部分のUI更新、画像組み込み

--- app/src/main/java/com/example/satohjohn/koten_chat/MainActivity.java
+++ app/src/main/java/com/example/satohjohn/koten_chat/MainActivity.java
@@ -1,20 +1,27 @@
 package com.example.satohjohn.koten_chat;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ScrollView;
+import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
@@ -24,7 +31,7 @@ import java.util.Locale;
 
 import static java.util.Locale.getDefault;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnKeyListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,35 +40,95 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         Button send_button = (Button)findViewById(R.id.send_button);
         send_button.setOnClickListener(this);
+
+        EditText editText = (EditText)findViewById(R.id.editText);
+        editText.setOnKeyListener(this);
+
+        Bitmap bm = null;
+        try {
+            InputStream is = getResources().getAssets().open("base_1024.png");
+            bm = BitmapFactory.decodeStream(is);
+            //bm = bm.createScaledBitmap(bm, 256, 256, false);
+        }
+        catch (IOException e) {
+            Log.d("Assets","Error");
+        }
+        ScrollView scrollView = (ScrollView)findViewById(R.id.scrollView);
+
+        BitmapDrawable ob = new BitmapDrawable(getResources(), bm);
+        scrollView.setBackground(ob);
+
+    }
+
+    public boolean onKey(View v, int keyCode, KeyEvent event) {
+        //EnterKeyが押されたかを判定
+        if (event.getAction() == KeyEvent.ACTION_DOWN  && keyCode == KeyEvent.KEYCODE_ENTER) {
+
+            //ソフトキーボードを閉じる
+            InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(v.getWindowToken(), 0);
+
+            onClick(v);
+
+            return true;
+        }
+        return false;
     }
 
     public void onClick(View v)
     {
         EditText comment = (EditText)findViewById(R.id.editText);
 
-        if( comment.getText().equals("") )
+        if( comment.getText().toString().equals("") )
         {
             return;
         }
 
+        Bitmap bm = null;
+
         // TableLayoutのグループを取得
         ViewGroup vg = (ViewGroup)findViewById(R.id.chat_table);
 
-        // 行を追加
-        getLayoutInflater().inflate(R.layout.talbe_row, vg);
-        // 文字設定
-        TableRow tr = (TableRow)vg.getChildAt(vg.getChildCount()-1);
+        if( vg.getChildCount() %2 == 0 ) {
+            try {
+                InputStream is = getResources().getAssets().open("icon_01.png");
+                bm = BitmapFactory.decodeStream(is);
+                bm = bm.createScaledBitmap(bm, 256, 256, false);
+            }
+            catch (IOException e) {
+                Log.d("Assets","Error");
+            }
 
-        Bitmap bm = null;
-        try {
-            InputStream is = getResources().getAssets().open("icon_01.jpg");
-            bm = BitmapFactory.decodeStream(is);
+            // 行を追加
+            getLayoutInflater().inflate(R.layout.talbe_row, vg);
+
+            // 文字設定
+            TableRow tr = (TableRow)vg.getChildAt(vg.getChildCount()-1);
+            ((ImageView)(tr.getChildAt(0))).setImageBitmap(bm);
+            ((TextView)(tr.getChildAt(1))).setText(comment.getText().toString());
         }
-        catch (IOException e) {
-            Log.d("Assets","Error");
+        else {
+            try {
+                InputStream is = getResources().getAssets().open("icon_02.png");
+                bm = BitmapFactory.decodeStream(is);
+                bm = bm.createScaledBitmap(bm, 256, 256, false);
+            }
+            catch (IOException e) {
+                Log.d("Assets","Error");
+            }
+
+            // 行を追加
+            getLayoutInflater().inflate(R.layout.talbe_row_right, vg);
+
+            // 文字設定
+            TableRow tr = (TableRow)vg.getChildAt(vg.getChildCount()-1);
+            ((ImageView)(tr.getChildAt(1))).setImageBitmap(bm);
+            ((TextView)(tr.getChildAt(0))).setText(comment.getText().toString());
         }
-        ((ImageView)(tr.getChildAt(0))).setImageBitmap(bm);
-        ((TextView)(tr.getChildAt(1))).setText(comment.getText());
+
         comment.setText("");
+
+        ScrollView scrollView = (ScrollView)findViewById(R.id.scrollView);
+        scrollView.fullScroll(ScrollView.FOCUS_DOWN);
     }
 }
