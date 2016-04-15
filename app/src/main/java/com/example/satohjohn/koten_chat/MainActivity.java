339c3292d6d643d1dd10fbc01b6bbdd1a1ff4b47
john <sato.keita@furyu.jp>
2016-04-15 15:42:45 +0900

    firebaseの追加

--- app/src/main/java/com/example/satohjohn/koten_chat/MainActivity.java
+++ app/src/main/java/com/example/satohjohn/koten_chat/MainActivity.java
@@ -75,6 +75,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         BitmapDrawable ob = new BitmapDrawable(getResources(), bm);
         scrollView.setBackground(ob);
 
+        Firebase.setAndroidContext(this);
+
+        myFirebaseRef = new Firebase("https://sweltering-fire-5633.firebaseio.com/");
+
+        myFirebaseRef.child("chats").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                for (DataSnapshot ds:  snapshot.getChildren()) {
+                    showText(ds.getValue().toString());
+//                    showText(value.message);
+                }
+                //System.out.println(snapshot.getValue());  //prints "Do you have data? You'll love Firebase."
+//                if (snapshot.getValue() != null) {
+////                    HashMap<String, Object> data = snapshot.getValue();
+//                    LinkedHashMap<Long, ArrayList<Chat>> value = (LinkedHashMap<Long, ArrayList<Chat>>) snapshot.getValue(new LinkedHashMap<Long, ArrayList<Chat>>().getClass());
+//                    showText(value.toString());
+//                }
+            }
+            @Override public void onCancelled(FirebaseError error) {
+                System.out.print("error = [" + error + "]");
+            }
+        });
+
     }
 
     public boolean onKey(View v, int keyCode, KeyEvent event) {
@@ -151,46 +174,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         ScrollView scrollView = (ScrollView)findViewById(R.id.scrollView);
         scrollView.fullScroll(ScrollView.FOCUS_DOWN);
+        myFirebaseRef.child("chats").push().setValue(changeWord);
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Firebase.setAndroidContext(this);
-
-        myFirebaseRef = new Firebase("https://sweltering-fire-5633.firebaseio.com/");
-
-        myFirebaseRef.child("chats").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot snapshot) {
-                for (DataSnapshot ds:  snapshot.getChildren()) {
-                    showText(ds.getValue().toString());
-//                    showText(value.message);
-                }
-                //System.out.println(snapshot.getValue());  //prints "Do you have data? You'll love Firebase."
-//                if (snapshot.getValue() != null) {
-////                    HashMap<String, Object> data = snapshot.getValue();
-//                    LinkedHashMap<Long, ArrayList<Chat>> value = (LinkedHashMap<Long, ArrayList<Chat>>) snapshot.getValue(new LinkedHashMap<Long, ArrayList<Chat>>().getClass());
-//                    showText(value.toString());
-//                }
-            }
-            @Override public void onCancelled(FirebaseError error) {
-                System.out.print("error = [" + error + "]");
-            }
-        });
-
-        Button button = (Button) findViewById(R.id.send_button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                myFirebaseRef.child("chats").setValue(new Chat("hogehoge", new Date().toString()));
-            }
-        });
-    }
     private void showText(String text) {
-        TextView te = (TextView) findViewById(R.id.text);
-        if (te != null) {
-            te.setText(text);
+
+        // TableLayoutのグループを取得
+        ViewGroup vg = (ViewGroup)findViewById(R.id.chat_table);
+
+        // 行を追加
+        getLayoutInflater().inflate(R.layout.talbe_row, vg);
+        // 文字設定
+        TableRow tr = (TableRow)vg.getChildAt(vg.getChildCount()-1);
+
+        Bitmap bm = null;
+        try {
+            InputStream is = getResources().getAssets().open("icon_01.jpg");
+            bm = BitmapFactory.decodeStream(is);
         }
+        catch (IOException e) {
+            Log.d("Assets","Error");
+        }
+        ((ImageView)(tr.getChildAt(0))).setImageBitmap(bm);
+        ((TextView)(tr.getChildAt(1))).setText(text);
     }
 }
