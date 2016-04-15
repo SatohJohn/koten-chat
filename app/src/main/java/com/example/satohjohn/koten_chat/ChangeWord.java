c1d28f21d6fdf17d472143e4b9f5800770de9c8b
土田 哲史 <0FR114036@local.furyu.jp>
2016-04-15 15:14:50 +0900

    たださんからの文字列繁栄

--- app/src/main/java/com/example/satohjohn/koten_chat/ChangeWord.java
+++ app/src/main/java/com/example/satohjohn/koten_chat/ChangeWord.java
@@ -16,6 +16,32 @@ public class ChangeWord {
     {
         kogoList.clear();
 
+        kogoList.add(new ChangeWordInfo("私の彼氏とってもいいかげんなの","わがめづの方、げにおろかなり"));
+        kogoList.add(new ChangeWordInfo("ねこがかわいい","ねこまうるわしきかな"));
+        kogoList.add(new ChangeWordInfo("おなかすいた","おなかすゐき"));
+        kogoList.add(new ChangeWordInfo("遊びにいこう","明日遊びにいかむ"));
+        kogoList.add(new ChangeWordInfo("いいかげん","おろかなり"));
+        kogoList.add(new ChangeWordInfo("いやだ"," いとほし"));
+        kogoList.add(new ChangeWordInfo("かわいいね","うるわしきかな"));
+        kogoList.add(new ChangeWordInfo("今っぽい","いまめかし"));
+        kogoList.add(new ChangeWordInfo("さっきの","ありつる"));
+        kogoList.add(new ChangeWordInfo("ざんねんだ","ほいなし"));
+        kogoList.add(new ChangeWordInfo("じつに","げに"));
+        kogoList.add(new ChangeWordInfo("それぞれ","おのがじし"));
+        kogoList.add(new ChangeWordInfo("たいせつだ","やむごとなし"));
+        kogoList.add(new ChangeWordInfo("つまらない","あぢきなし"));
+        kogoList.add(new ChangeWordInfo("ふかいだ","いぶせし"));
+        kogoList.add(new ChangeWordInfo("ほんとうに","げに,いと"));
+        kogoList.add(new ChangeWordInfo("まちどおしい","こころもとなし"));
+        kogoList.add(new ChangeWordInfo("むだだ","いたづらなり"));
+        kogoList.add(new ChangeWordInfo("やっと","やうやう"));
+        kogoList.add(new ChangeWordInfo("わたし","われ"));
+        kogoList.add(new ChangeWordInfo("あいする","めづ "));
+        kogoList.add(new ChangeWordInfo("ねこ","ねこま"));
+        kogoList.add(new ChangeWordInfo("おもむきがある", "おかし"));
+        kogoList.add(new ChangeWordInfo("おもしろい", "おかし"));
+        kogoList.add(new ChangeWordInfo("まじで", "いと"));
+        kogoList.add(new ChangeWordInfo("とても", "いと"));
         kogoList.add(new ChangeWordInfo("おもむきがある", "おかし"));
         kogoList.add(new ChangeWordInfo("おもしろい", "おかし"));
         kogoList.add(new ChangeWordInfo("まじで", "いと"));
